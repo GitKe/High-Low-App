d1b35ee771ca79dbbee9f0a448917a1abbca4226
Peter Ke <kepeterphysics@yahoo.com>
2017-03-13 21:06:25 +0000

    Randomize the number

--- java.js
+++ java.js
@@ -1,18 +1,16 @@
 $(document).ready(function() {
-
+    var num = Math.floor(Math.random() * 100);
     $("#button").click(function() {
         var name1 = $("#input").val()
         var name2 = parseInt(name1)
-        $("p").html(name2);
-        
-        var num = Math.floor((Math.random() * 10));
-        if (num>name1) {
+      
+        if (num>name2) {
             $("p").html("The number is too low!");
         }
-        else if (num == name1) {
+        else if (num === name2) {
             $("p").html("The guess is correct!");
         }
-        else if (num =! name1) {
+        else if (num ==! name2) {
             $("p").html("This is not a number!");
         }
         else {
@@ -20,6 +18,6 @@ $(document).ready(function() {
         }
     
         })
-        
+    
         
 });
